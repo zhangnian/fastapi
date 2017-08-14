9cff82941a2a67ff9b273d83d22bf6e9e55b352a
zhangnian <zhangnian@91jianke.com>
2017-08-14 17:26:51 +0800

    fix:修改配置文件的默认配置

--- fastapi/config/default.py
+++ fastapi/config/default.py
@@ -9,9 +9,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///user.db'
 
     ENABLE_SENTRY = False
-    SENTRY_DSN = 'https://528bd615fd8e42beacf603d6270c99c8:6c4886cc414f4cd9b0045bfeeac3f1e5@sentry.io/127902'
+    SENTRY_DSN = ''
 
-    REDIS_HOST = 'r-bp1507482787ce14.redis.rds.aliyuncs.com'
+    REDIS_HOST = 'localhost'
     REDIS_PORT = 6379
     REDIS_DB = 0
-    REDIS_PASSWORD = 'Jhy731005'
+    REDIS_PASSWORD = 'redis'
